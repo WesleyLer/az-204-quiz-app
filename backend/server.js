870abb6cf10121caad88352ac92b20d036548b2c
WesleyLer <Wesley.Ler@au.ey.com>
2025-09-14 18:13:56 +1000

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const cors = require('cors');
 const fs = require('fs');
 const path = require('path');
-const env = require('dotenv').config();
+require('dotenv').config();
 
 const app = express();
 const PORT = process.env.PORT || 3001;
